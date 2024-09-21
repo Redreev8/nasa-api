93d22761c9760166b52919e3b660cca7d1f4682b
redreev8 <redreevs@mail.ru>
2024-09-21 23:25:36 +0500

    Api: update get-day add filters

--- src/api/nasa/day/get-day.ts
+++ src/api/nasa/day/get-day.ts
@@ -1,10 +1,12 @@
+import getFilterUrl, { FiltersUrl } from '@/helper/filter-url'
 import url, { NasaData } from './index'
 
-export default async (optinal?: RequestInit): Promise<NasaData | string> => {
+
+
+export default async (optinal?: RequestInit, filtersUrl?: FiltersUrl): Promise<NasaData | string> => {
     'use server'
     try {
-        
-        const res = await fetch(url, optinal)
+        const res = await fetch(url + (filtersUrl ? getFilterUrl(filtersUrl!, '&') : ''), optinal)
         return res.json()
     } catch (err) {
         console.log(err);
