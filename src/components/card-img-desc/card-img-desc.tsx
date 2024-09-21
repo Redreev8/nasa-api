eef67fa005ee66f73af70c9061cd8c0a1acf05b4
redreev8 <redreevs@mail.ru>
2024-09-21 23:19:42 +0500

    component: add card-img-desc

--- src/components/card-img-desc/card-img-desc.tsx
+++ src/components/card-img-desc/card-img-desc.tsx
@@ -3,26 +3,22 @@ import style from './card-img-desc.module.css'
 import Paragraf from "@/ui/paragraf";
 import Title from "@/ui/title";
 import TextBackground from "@/ui/text-background";
-export interface CardImgDescProps {
+import Img from '@/type/img';
+export interface CardImgDescProps extends Img {
+    title: string
+    children: string
 }
 
-const CardImgDesc: FC<CardImgDescProps> = ({}) => {
+const CardImgDesc: FC<CardImgDescProps> = ({ title, children, src, alt }) => {
     return (
 		<div className={ style['card'] }>
-			<img className={ style['img'] } src="https://apod.nasa.gov/apod/image/2409/Mermaid_Corke_1080.jpg" alt="" />
+			<img className={ style['img'] } src={ src } alt={ alt } />
 			<Title className={ style['title'] } level={2} levelTag={2}>
-				<TextBackground>The Mermaid Nebula Supernova Remnant</TextBackground>
+				<TextBackground>{ title }</TextBackground>
 			</Title>
 			<Paragraf className={ style['text'] }>
 				<TextBackground classNameContent={ style['text__wrapp'] }>
-					New stars are born from the remnants of dead stars. The gaseous remnant of the gravitational 
-					collapse and subsequent death of a very massive star in our Milky Way created the G296.5+10.0
-					supernova remnant, of which the featured Mermaid Nebula is part. Also known as the Betta Fish
-					Nebula, the Mermaid Nebula makes up part of an unusual subclass of supernova remnants that are
-					two-sided and nearly circular. Originally discovered in X-rays, the filamentary nebula is a 
-					frequently studied source also in radio and gamma-ray light. The blue color visible here originates
-					from doubly ionized oxygen (OIII), while the deep red is emitted by hydrogen gas. The nebula's
-					mermaid-like shape has proven to be useful for measurements of the interstellar magnetic field.
+					{ children }
 				</TextBackground>
 			</Paragraf>
 		</div>
