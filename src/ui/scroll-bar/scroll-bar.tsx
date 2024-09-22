93116e5c8c844be50abe54fed820ae020d7daa6f
redreev8 <redreevs@mail.ru>
2024-09-22 23:39:52 +0500

    UI: scroll-bar update osi x adn y

--- src/ui/scroll-bar/scroll-bar.tsx
+++ src/ui/scroll-bar/scroll-bar.tsx
@@ -2,12 +2,18 @@
 import { AreaHTMLAttributes, FC, ReactNode, TouchEvent, useRef, useState, WheelEvent } from 'react'
 import style from './scroll-bar.module.scss'
 import classNames from 'classnames'
-export interface ScrollBarProps extends AreaHTMLAttributes<HTMLDivElement> {
-    children: ReactNode
+export interface ScrollBarProps {
+    onToScroll?: (obj: {
+        direction: number
+        scroll: number
+    }) => void
+    valueScrool?: number
     initScroll?: number
+    os?: 'X' | 'Y'
 }
 
-const ScrollBar: FC<ScrollBarProps> = ({ children, initScroll=200, className, ...props }) => {
+const ScrollBar: FC<ScrollBarProps & AreaHTMLAttributes<HTMLDivElement>> = 
+({ children, initScroll=0, onToScroll, className, os = 'X', valueScrool = 16, ...props }) => {
     const refBar = useRef<HTMLDivElement>(null)
     const refWrapp = useRef<HTMLDivElement>(null)
     const [scroll, setScroll] = useState<number>(initScroll)
@@ -15,25 +21,26 @@ const ScrollBar: FC<ScrollBarProps> = ({ children, initScroll=200, className, ..
         prev: 0
     })
     const hendalScroll = ({ deltaY }: WheelEvent<HTMLDivElement>) => {
-        toScroll(deltaY / -100)
+        toScroll(deltaY / 100)
     }
-    const toScroll = (n: number)=> {
+    const toScroll = (direction: number)=> {
         setScroll(prev => {
-            const newValue = prev + (n * 16)
+            let newValue = prev + (direction * valueScrool)
             const maxValue = refWrapp.current!.clientWidth - refBar.current!.clientWidth
-            if (newValue < 0) return 0
-            if (newValue > maxValue) return maxValue
+            if (newValue < 0) newValue = 0
+            if (newValue > maxValue) newValue = maxValue
+            onToScroll && onToScroll({ direction, scroll: newValue })
             return newValue
         })
     }
     const hendalTouch = (e: TouchEvent<HTMLDivElement>) => {
-        if(e.touches[0].clientX < refTouch.current.prev) toScroll(1)
-        if(e.touches[0].clientX > refTouch.current.prev) toScroll(-1)
-        refTouch.current.prev = e.touches[0].clientX
+        if(e.touches[0][`client${os}`] < refTouch.current.prev) toScroll(1)
+        if(e.touches[0][`client${os}`] > refTouch.current.prev) toScroll(-1)
+        refTouch.current.prev = e.touches[0][`client${os}`]
     }
     return (
         <div ref={ refBar } onWheel={ hendalScroll } onTouchMove={hendalTouch} className={ classNames(style['scroll-bar'], className) } { ...props }>
-            <div style={{ transform: `translateX(${-scroll}px)` }} ref={ refWrapp } className={ classNames(style['wrapp']) }>
+            <div style={{ transform: `translate${os}(${-scroll}px)` }} ref={ refWrapp } className={ classNames(style['wrapp']) }>
                 { children }
             </div>
         </div>
