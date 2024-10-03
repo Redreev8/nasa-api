c18aec291c8e314becba5499e4abdcb0a62d2031
redreev8 <redreevs@mail.ru>
2024-10-03 15:53:30 +0500

    style: change size h6

--- src/ui/title/title.tsx
+++ src/ui/title/title.tsx
@@ -18,6 +18,7 @@ const levels: Levels = {
     3: forwardRef<HTMLHeadingElement, TitleProps>(({ children, ...props }, ref) => <h3 ref={ ref } { ...props }>{ children }</h3>),
     4: forwardRef<HTMLHeadingElement, TitleProps>(({ children, ...props }, ref) => <h4 ref={ ref } { ...props }>{ children }</h4>),
     5: forwardRef<HTMLHeadingElement, TitleProps>(({ children, ...props }, ref) => <h5 ref={ ref } { ...props }>{ children }</h5>),
+    6: forwardRef<HTMLHeadingElement, TitleProps>(({ children, ...props }, ref) => <h6 ref={ ref } { ...props }>{ children }</h6>),
 }
 
 const Title = forwardRef<HTMLHeadingElement, TitleProps>(({ className, level=2, levelTag=level, children, ...props }, ref) => {
